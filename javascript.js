b7f36668da19aeab92951ff3ae9f1d13b1606d29
arianchhim978 <147645634+arianchhim978@users.noreply.github.com>
2024-03-18 18:00:00 -0400

    making a mobile friendly first site

--- javascript.js
+++ javascript.js
@@ -8,3 +8,26 @@ button.addEventListener("click", function() {
         header.style.display = "none";
     }
 });
+
+
+
+const trialButtons = document.querySelectorAll('.trial, .trial2, .trial3');
+
+// Function to handle button click
+function handleClick(event) {
+    // Reset the color of all buttons
+    trialButtons.forEach(button => {
+        button.style.backgroundColor = "whitesmoke"; // Change to default color
+    });
+    
+    // Set the background color of the clicked button
+    event.target.style.backgroundColor = "grey";
+}
+
+// Add event listener to each trial button
+trialButtons.forEach(button => {
+    button.addEventListener('click', handleClick);
+});
+
+// Click the first trial button by default
+document.querySelector('.trial').click();
